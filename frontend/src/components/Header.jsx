dd9ce3d0c259fcab436802c6168641573df6b1ce
Annie <cung.ngananh@gmail.com>
2023-09-13 17:27:03 -0700

    Add header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import {Navbar, Nav, Container, NavLink} from 'react-bootstrap';
+import {FaShoppingCart, FaUser} from 'react-icons/fa';
+
+const Header = () => {
+  return (
+    <header>
+        <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+            <Container>
+                <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <NavLink href="/cart"><FaShoppingCart />Cart</NavLink>
+                        <NavLink href="/login"><FaUser />Sign In</NavLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </header>
+  )
+}
+
+export default Header
